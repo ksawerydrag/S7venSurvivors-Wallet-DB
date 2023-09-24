588432f0429e4d61fe643b8bca5b6974dedde4b5
ksawerythornton <115893688+KsaweryThornton@users.noreply.github.com>
2023-09-24 13:47:47 +0200

    getTransactions

--- src/service/transactions.service.js
+++ src/service/transactions.service.js
@@ -3,3 +3,10 @@ const Transaction = require("../models/transaction.model");
 const getTransactions = async (query) => {
   return Transaction.find(query);
 };
+
+module.exports = {
+  getTransactions,
+  // getCategories,
+  // getMore,
+  // createTransaction
+};
