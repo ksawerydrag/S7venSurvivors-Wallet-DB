0e18e754ec596c584c0fe8c33dc40a7b407b19dd
ksawerythornton <115893688+KsaweryThornton@users.noreply.github.com>
2023-09-24 13:52:40 +0200

    fixed

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const transactionsController = require("../../controllers/transactions.controllers");
-const auth = require("../../middlewares/auth");
+const transactionsController = require("../controllers/transactions.controllers");
+const auth = require("../middlewares/auth");
 
 router.get("/", auth, transactionsController.get);
 
